a3e503e52148aa2099bde3792bf467395c141c5f
sabinasattarova <sabiwasf@gmail.com>
2023-10-30 15:56:29 -0400

    updates

--- src/test/java/com/dara/pages/Home_driver_page_dara.java
+++ src/test/java/com/dara/pages/Home_driver_page_dara.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.PageFactory;
 
 public class Home_driver_page_dara {
 
+    //for driver user home page POM
+
     public Home_driver_page_dara(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
--- src/test/java/com/dara/pages/Home_page_dara.java
+++ src/test/java/com/dara/pages/Home_page_dara.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.PageFactory;
 
 public class Home_page_dara {
 
+    // for store manager and sales manager users POM
+
     public Home_page_dara(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
