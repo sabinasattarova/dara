d20f0d0173e0f8de6707bc55a390b20a4e719136
sabinasattarova <sabiwasf@gmail.com>
2023-10-30 15:45:15 -0400

    setup HomePage

--- src/test/java/com/dara/pages/Home_page_dara.java
+++ src/test/java/com/dara/pages/Home_page_dara.java
@@ -1,4 +1,15 @@
 package com.dara.pages;
 
+import com.dara.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public class Home_page_dara {
+
+    public Home_page_dara(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+
+
 }
