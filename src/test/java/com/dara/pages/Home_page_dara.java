fe1d6b6f5c7004b1ea34d52472248b82c8e1f49a
elvegl <v.cutied@gmail.com>
2023-10-31 03:24:39 -0400

    Added @FindBy for marketing page and tab

--- src/test/java/com/dara/pages/Home_page_dara.java
+++ src/test/java/com/dara/pages/Home_page_dara.java
@@ -1,4 +1,21 @@
 package com.dara.pages;
 
+import com.dara.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Home_page_dara {
+
+    public Home_page_dara(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//a[@href='/campaign/']/span/..")
+    public WebElement marketingPage;
+
+    @FindBy(xpath = "//*[@id=\"main-menu\"]/ul/li[6]")
+    public WebElement marketingTab;
+
+
 }
