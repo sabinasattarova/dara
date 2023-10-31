76f0f3394c6fa497b892a3e8590ddfbc5e9c2e32
sabinasattarova <sabiwasf@gmail.com>
2023-10-31 14:57:16 -0400

    wip US6 created feature file and stepDef

---
+++ src/test/java/com/dara/step_definition/Edit_car_info_icons_SS.java
@@ -0,0 +1,23 @@
+package com.dara.step_definition;
+
+import io.cucumber.java.en.Then;
+
+public class Edit_car_info_icons_SS {
+
+    @Then("user chooses {string} from Fleet drop-down")
+    public void user_chooses_from_fleet_drop_down(String string) {
+
+    }
+    @Then("user hovers over on three dots on car table line following options should appear")
+    public void user_hovers_over_on_three_dots_on_car_table_line_following_options_should_appear(io.cucumber.datatable.DataTable dataTable) {
+
+    }
+    @Then("user sees error msg {string}")
+    public void user_sees_error_msg(String string) {
+
+    }
+
+
+
+
+}
