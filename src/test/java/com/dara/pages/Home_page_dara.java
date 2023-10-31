ea1ddd48c4b9d1c7de1ca67eb7b8a1954d823fb0
agil1402b <agil.itu@gmail.com>
2023-10-30 20:34:28 -0400

    added 1 WebElement to Home_page_dara

--- src/test/java/com/dara/pages/Home_page_dara.java
+++ src/test/java/com/dara/pages/Home_page_dara.java
@@ -1,6 +1,8 @@
 package com.dara.pages;
 
 import com.dara.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Home_page_dara {
@@ -10,8 +12,8 @@ public class Home_page_dara {
     public Home_page_dara(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
-
-
+    @FindBy(xpath = "(//span[@class='title title-level-1'])[2]")
+    public WebElement Fleet;
 
 
 }
