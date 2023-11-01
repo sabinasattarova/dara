8113afbb34360e89cd8b0d61f30bbaa3a94b600d
agil1402b <agil.itu@gmail.com>
2023-11-01 19:15:47 -0400

    updated feature file

--- src/test/java/com/dara/pages/Vehicle_cost_page_AB.java
+++ src/test/java/com/dara/pages/Vehicle_cost_page_AB.java
@@ -16,14 +16,15 @@ public class Vehicle_cost_page_AB {
     public WebElement vehicleCosts;
 
 
+    @FindBy(xpath = "//input[@data-role='select-row-cell']")
+    public List<WebElement> vehicleCostCheckBoxes;
+
     @FindBy(xpath = "//a[@class='grid-header-cell__link']")
     public List<WebElement> columns;
 
-    @FindBy(xpath = "(//input[@type='checkbox'])[4]")
-    public WebElement typeCheckbox;
 
-    @FindBy(xpath = "//input[@data-role='select-row-cell']")
-    public List<WebElement> vehicleCostCheckBoxes;
+    @FindBy(xpath = "//button[@class='btn btn-default btn-small dropdown-toggle']/input")
+    public WebElement typeCheckbox;
 
 
 
