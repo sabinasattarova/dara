41a2af1d45e3e26a1a256dc3a168379e53e350f7
sabinasattarova <sabiwasf@gmail.com>
2023-11-01 16:48:12 -0400

    wip US6 stepDef, pages

--- src/test/java/com/dara/pages/VehicleModels_page_AZ.java
+++ src/test/java/com/dara/pages/VehicleModels_page_AZ.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class VehicleModels_page_AZ {
@@ -23,8 +24,30 @@ public class VehicleModels_page_AZ {
     @FindBy(xpath = "//tbody[@class='grid-body']/tr[1]//div[@class='more-bar-holder']/div")
     public WebElement editDropdownDots;
 
-    @FindBy(xpath = "//tbody[@class='grid-body']/tr[1]//div[@class='more-bar-holder']/div//li[@class='launcher-item']")
-    public List<WebElement> viewEditDeleteOpt;
+    @FindBy(xpath = "//a[@title='View']")
+    public WebElement viewOpt;
+
+    @FindBy(xpath = "//a[@title='Edit']")
+    public WebElement editOpt;
+
+    @FindBy(xpath = "//a[@title='Delete']")
+    public WebElement deleteOpt;
+
+
+
+
+
+
+
+    public List<WebElement> getEditDropdownOpt(){
+
+        List<WebElement> all = new ArrayList<>();
+        all.add(viewOpt);
+        all.add(editOpt);
+        all.add(deleteOpt);
+
+        return all;
+    }
 
 
 }
