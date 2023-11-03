f0eb8577a260c2dd02a0fa67a7d13e2e9f72e3dd
KevinV <kvillacis7@gmail.com>
2023-11-02 21:00:51 -0400

    US9-Finished User Story

--- src/test/java/com/dara/step_definition/RepeatDayErrorMessage_stepDef_KV.java
+++ src/test/java/com/dara/step_definition/RepeatDayErrorMessage_stepDef_KV.java
@@ -3,7 +3,14 @@ package com.dara.step_definition;
 import com.dara.pages.CalendarEvents_page_KV;
 import com.dara.pages.Home_driver_page_dara;
 import com.dara.pages.Home_page_dara;
+import com.dara.utilities.BrowserUtils;
+import com.dara.utilities.Driver;
 import io.cucumber.java.en.Then;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotInteractableException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class RepeatDayErrorMessage_stepDef_KV {
 
@@ -12,23 +19,16 @@ public class RepeatDayErrorMessage_stepDef_KV {
     Home_page_dara homepage = new Home_page_dara();
 
 
-    @Then("user clicks on Activities")
-    public void user_clicks_on_activities() {
 
-
-        driverPageDara.activitiesTab.click();
-
-    }
-    @Then("user clicks on Calendar Events")
-    public void user_clicks_on_calendar_events() {
-
-        driverPageDara.calendarEventsTab.click();
-
-    }
     @Then("user clicks on Create Calendar Event")
     public void user_clicks_on_create_calendar_event() {
 
-        calendarEvents.createCalenderEventTab.click();
+        BrowserUtils.waitForInvisibilityOf(Driver.getDriver().findElement(By.xpath("/html/body/div[4]/div")));
+        try {
+            calendarEvents.createCalenderEventTab.click();
+        }catch (NullPointerException e){
+            calendarEvents.createCalenderEventTab.click();
+        }
 
     }
     @Then("user clicks on Repeat checkbox")
@@ -40,14 +40,18 @@ public class RepeatDayErrorMessage_stepDef_KV {
     @Then("user enters integer less than {int}")
     public void userEntersIntegerLessThan(int negativeNumber) {
 
+        calendarEvents.repeatEveryInputBox.clear();
         calendarEvents.repeatEveryInputBox.sendKeys(""+negativeNumber);
+        calendarEvents.LessThan1ErrorMessage.isDisplayed();
 
     }
 
     @Then("user enters integer more than {int}")
     public void userEntersIntegerMoreThan(int NumberOver100) {
 
+        calendarEvents.repeatEveryInputBox.clear();
         calendarEvents.repeatEveryInputBox.sendKeys(""+NumberOver100);
+        calendarEvents.MoreThan99ErrorMessage.isDisplayed();
 
     }
 
@@ -68,8 +72,10 @@ public class RepeatDayErrorMessage_stepDef_KV {
 
     @Then("{string} clicks on Activities")
     public void clicksOnActivities(String userType) {
-
-        driverPageDara.chooseDropdownOption(driverPageDara.activitiesTab, driverPageDara.activitiesTabDropDown,"Calendar Events");
-        homepage.chooseDropdownOption(homepage.activitiesTab, homepage.activitiesTabDropDown,"Calendar Events");
+        if (userType.equalsIgnoreCase("driver")){
+            driverPageDara.chooseDropdownOption(driverPageDara.activitiesTab, driverPageDara.activitiesTabDropDown,"Calendar Events");
+        }else{
+            homepage.chooseDropdownOption(homepage.activitiesTab, homepage.activitiesTabDropDown,"Calendar Events");
+        }
     }
 }
